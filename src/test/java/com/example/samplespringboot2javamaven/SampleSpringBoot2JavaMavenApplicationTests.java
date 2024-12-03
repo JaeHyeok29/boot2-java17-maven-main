6afc0f8f0fa32f4767df113c6f1294f9ba480549
Jae Hyeok <rlawogur329@mail.ulsan.ac.kr>
2024-12-03 11:45:29 +0900

    [Feat]: first commit

---
+++ src/test/java/com/example/samplespringboot2javamaven/SampleSpringBoot2JavaMavenApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.samplespringboot2javamaven;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class SampleSpringBoot2JavaMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
